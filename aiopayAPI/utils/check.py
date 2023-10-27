020286a8cfc9c92fd2685adfed57e70f3fb700cb
xllebbSQ <090504opo@gmail.com>
2023-10-27 20:11:07 +0300

    примеры использования

--- aiopayAPI/utils/check.py
+++ aiopayAPI/utils/check.py
@@ -3,7 +3,7 @@ from typing import Dict, Callable
 import time
 
 class Checker:
-    """Проверка данных """
+    """Проверка данных"""
     def __init__(self, **params: Dict | None) -> None:
         self.params = params
 
@@ -28,33 +28,7 @@ class Checker:
         
 
    
-    # def status_wait(status: int = 1, update_time: int = 2, object_with_data: Dict = None) -> Callable:
-    #         """
-    #         Декоратор, который проверяет статус транзакции. Если оно равно 1, то функция выполняется.
-        
-    #         :param status: Статус транзакции (default=1)
-    #         :type status: int
-        
-    #         :param update_time: Время обновления запроса в секундах (default=2)
-    #         :type update_time: int, seconds
-        
-    #         :return: Функцию
-    #         :rtype: Callable
-    #         """
-    #         def decorator(func: Callable) -> Callable:
-    #             trans_status = object_with_data["1"]["transaction_status"]
-        
-    #             def wrapper():
-    #                 while True:
-    #                     if trans_status != 1:
-    #                         time.sleep(update_time)
-    #                     else:
-    #                         func()
-    #                         break
-                
-    #             return wrapper
-        
-    #         return decorator
+
     
 
 
