51da98b47733ead61735f40971640496faa3590a
xllebbSQ <090504opo@gmail.com>
2023-10-29 01:57:06 +0300

    Изменил setup.py

--- examples/balance.py
+++ examples/balance.py
@@ -1,11 +1,35 @@
-from aiopayAPI import PayOk
+from pydantic import BaseModel
+from aiopayAPI.payok import GetAll, PayOk
 
-import asyncio
 
-async def main():
-    pay = PayOk(
-        API_ID=1111,
-        API_KEY=2222,
-        shop=3333
-    )
-    await pay.get.balance()
+
+        
+
+class Get:
+    def __init__(self) -> None:
+        self.some: Some = Some()
+
+    def get_name(self):
+        return self.some.name
+
+class Some(BaseModel):
+    """
+    :param name: Имя
+    :type name: :obj:`str`
+    """
+    name: str = "John"
+    """Имя
+    :type name: :obj:`str`
+    """
+
+
+    def get_name(self):
+        return self.name
+
+
+some = Some(name="Mike")
+print(some.get_name())
+
+
+
+
